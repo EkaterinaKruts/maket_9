4f1e4809b10aeec1537d50ff0cb5b3c36f251d24
Ekaterina <katerunka90@gmail.com>
2018-08-15 01:18:04 +0300

    d

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
-function touchRock() {
-  var userName = prompt("Как вас зовут?", "Введите ваше имя.");
-  if (userName) {
-  setTimeout(function(){ alert("Рад вас видеть, " + userName + ".")}, 100);
-  document.getElementById("rockImg").src = "images/rock happy.png"}
-  setTimeout("document.getElementById('rockImg').src ='images/rock lonely.png';", 60000);
+  var userName;
+  function greetUser () {
+    alert ('Hello, I am your pet, Rock!');
+  }
 
-}
+  function touchRock () {
+
+      if (userName) {
+          alert("Рад вас видеть, " + userName + ". Спасибо.");
+        }
+      else {
+        userName = prompt ('What is your name?', 'Enter your name here.');
+        if (userName)
+          alert ('It is good to meet you, ' + userName + '.');
+      }
+
+          document.getElementById("rockImg").src = "images/rock happy.png";
+          setTimeout("document.getElementById('rockImg').src ='images/rock lonely.png';", 60000);
+
+    }
