ea752bf6914dcfd4022569d0239c2c11e8e2574c
Ekaterina <katerunka90@gmail.com>
2018-08-15 00:17:26 +0300

    ffff

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 function touchRock() {
   var userName = prompt("Как вас зовут?", "Введите ваше имя.");
   if (userName) {
-  alert("Рад вас видеть, " + userName + ".");
+  setTimeout(function(){ alert("Рад вас видеть, " + userName + ".")}, 100);
   document.getElementById("rockImg").src = "images/rock happy.png"}
+  setTimeout("document.getElementById('rockImg').src ='images/rock lonely.png';", 60000);
 
 }
