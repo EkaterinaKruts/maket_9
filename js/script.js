711cf562b4d7dcd78004888749b74a012b6211ae
Ekaterina <katerunka90@gmail.com>
2018-08-17 23:48:40 +0300

    ggggg

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 
   var userName;
   function greetUser () {
+    if (navigator.cookieEnabled)
+    userName = readCookie ("irock_username");
+    if (userName)
+    alert ("Hello" + userName + ", I missed you.");
+    else
     alert ('Hello, I am your pet, Rock!');
   }
 
@@ -13,7 +18,14 @@
         userName = prompt ('What is your name?', 'Enter your name here.');
         if (userName)
           alert ('It is good to meet you, ' + userName + '.');
-      }
+          if (navigator.cookieEnabled)
+          writeCookie("irock_username", userName, 5 * 365);
+          else
+              alert ("Sorry. Cookies aren't supported/enabled in your browser. I won't remember you later.")
+            }
+
+
+
 
           document.getElementById("rockImg").src = "images/rock happy.png";
           setTimeout("document.getElementById('rockImg').src ='images/rock lonely.png';", 60000);
